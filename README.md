d5f9ca1068eb42c7260b970acb00146cc25253d1
Jan Neumann <66623604+janeumnn@users.noreply.github.com>
2020-12-18 21:58:11 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.\
+You will also see any lint errors in the console.
+
+### `npm test`
+
+Launches the test runner in the interactive watch mode.\
+
+### `npm run build`
+
+Builds the app for production to the `build` folder.\
+It correctly bundles React in production mode and optimizes the build for the best performance.
+
+The build is minified and the filenames include the hashes.\
+Your app is ready to be deployed!
+
+### `npm run build:openssl`
+
+Runs the build script in the `openssl` folder and builds the OpenSSL command line in WebAssembly.\
+Requires `emscripten` environment!
+
+### `npm run eject`
+
+**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
+
+You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature.\
+If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
---
+++ openssl/build.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+OPENSSL_VERSION="openssl-3.0.0-alpha9"
+OPENSSL_DIR=${OPENSSL_VERSION}
+OPENSSL_JS_PATH="../src/openssl.js"
+OPENSSL_WASM_PATH="../public/openssl.wasm"
+
+if [ -d ${OPENSSL_DIR} ]; then
+  rm -rf ${OPENSSL_DIR}
+fi
+
+if [ -f ${OPENSSL_JS_PATH} ]; then
+  rm -rf ${OPENSSL_JS_PATH}
+fi
+
+if [ -f ${OPENSSL_WASM} ]; then
+  rm -rf ${OPENSSL_WASM}
+fi
+
+if [ ! -f ${OPENSSL_VERSION}.tar.gz ]; then
+  curl -O https://www.openssl.org/source/${OPENSSL_VERSION}.tar.gz
+fi
+
+mkdir ${OPENSSL_DIR}
+tar xf ${OPENSSL_VERSION}.tar.gz --strip-components=1 --directory=${OPENSSL_DIR}
+cd ${OPENSSL_DIR} || exit 1
+
+export CC=emcc
+export CXX=emcc
+export LDFLAGS="\
+  -s ENVIRONMENT='web'\
+  -s FILESYSTEM=1\
+  -s MODULARIZE=1\
+  -s EXPORT_NAME=OpenSSL\
+  -s EXTRA_EXPORTED_RUNTIME_METHODS=\"['run', 'callMain', 'FS']\"\
+  -s INVOKE_RUN=0\
+  -s EXPORT_ES6=1\
+  -s USE_ES6_IMPORT_META=0"
+
+emconfigure ./Configure no-hw no-shared no-asm no-threads -static
+
+make apps/progs.h
+
+sed -i 's/$(CROSS_COMPILE)//' Makefile
+
+emmake make -j 8 build_generated libssl.a libcrypto.a apps/openssl
+
+mv apps/openssl apps/openssl.js
+sed -i.old '1s;^;\/* eslint-disable *\/;' apps/openssl.js
+sed -i.old "s|openssl.wasm|/openssl.wasm|" apps/openssl.js
+sed -i.old "s|wasmBinaryFile=locateFile(wasmBinaryFile)||" apps/openssl.js
+cp apps/openssl.js ../../src/
+cp apps/openssl.wasm ../../public/
