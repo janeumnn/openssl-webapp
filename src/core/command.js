4b1b77f4b0e0c2ff00e6689a963289958638f56d
janeumnn <jan.neumann@ensun.de>
2020-12-30 00:33:25 +0100

    Change method signature and file handling

--- src/core/command.js
+++ src/core/command.js
@@ -10,42 +10,76 @@ class Command {
     return this.resultSubject.asObservable();
   }
 
-  async run(args, text = null, file = null) {
+  getByteArray(file) {
+    const fileReader = new FileReader();
+    return new Promise(function (resolve, reject) {
+      fileReader.readAsArrayBuffer(file);
+      fileReader.onload = function (event) {
+        const array = new Uint8Array(event.target.result);
+        const fileByteArray = [];
+        for (let i = 0; i < array.length; i++) {
+          fileByteArray.push(array[i]);
+        }
+        resolve(array);
+      };
+      fileReader.onerror = reject;
+    });
+  }
+
+  run(args, data) {
+    let inputText;
+    let inputFile;
+    let writeFileBuffer;
+
     let output = {
       stdout: '',
       stderr: '',
-      resultText: null,
-      resultFile: null,
+      text: null,
+      file: null,
     };
-
-    let writeFileBuffer;
-    if (file) {
-      writeFileBuffer = await file.arrayBuffer();
-    }
 
     const moduleObj = {
-      print: function (text) {
-        output.stdout += text + '\n';
+      print: function (line) {
+        output.stdout += line + '\n';
       },
-      printErr: function (text) {
-        output.stderr += text + '\n';
+      printErr: function (line) {
+        output.stderr += line + '\n';
       },
     };
 
+    const argsArray = this.convertArgsToArray(args);
+
+    if (args && !(Object.prototype.toString.call(args) === '[object String]')) {
+      return;
+    } else if (data && Object.prototype.toString.call(data) === '[object String]') {
+      inputText = data;
+    } else if (data && Object.prototype.toString.call(data) === '[object File]') {
+      inputFile = data;
+      this.getByteArray(inputFile).then((byteArray) => {
+        writeFileBuffer = byteArray;
+      });
+    }
+
     OpenSSL(moduleObj).then((module) => {
-      if (text) {
-        module['FS'].writeFile('file', text);
-      } else if (file) {
-        module['FS'].writeFile('file', new Uint8Array(writeFileBuffer));
+      if (inputText) {
+        module['FS'].writeFile(this.getFileInParameter(argsArray), inputText);
+      } else if (inputFile) {
+        module['FS'].writeFile(this.getFileInParameter(argsArray), writeFileBuffer);
       }
 
-      module.callMain(this.convertArgsToArray(args));
+      module.callMain(argsArray);
 
-      if (text) {
-        output.resultText = module['FS'].readFile('enc', { encoding: 'utf8' });
-      } else if (file) {
-        const readFileBuffer = module['FS'].readFile('output', { encoding: 'binary' });
-        output.resultFile = new Blob([readFileBuffer], { type: 'application/octet-stream' });
+      if (inputText) {
+        output.text = module['FS'].readFile(this.getFileOutParameter(argsArray), {
+          encoding: 'utf8',
+        });
+      } else if (inputFile) {
+        const readFileBuffer = module['FS'].readFile(this.getFileOutParameter(argsArray), {
+          encoding: 'binary',
+        });
+        output.file = new File([readFileBuffer], this.getFileOutParameter(argsArray), {
+          type: 'application/octet-stream',
+        });
       }
 
       this.resultSubject.next(output);
@@ -54,6 +88,14 @@ class Command {
 
   convertArgsToArray(args) {
     return args.split(/[\s]{1,}/g).filter(Boolean);
+  }
+
+  getFileInParameter(argsArray) {
+    return argsArray[argsArray.indexOf('-in') + 1];
+  }
+
+  getFileOutParameter(argsArray) {
+    return argsArray[argsArray.indexOf('-out') + 1];
   }
 }
 
