3d7133af4a6c02e73c986cc1f811ac7a9c366d4b
Jan Neumann <66623604+janeumnn@users.noreply.github.com>
2021-01-01 15:40:53 +0100

    Change test suite structure and add new test case

--- src/core/command.test.js
+++ src/core/command.test.js
@@ -1,64 +1,66 @@
 import Command from './command';
+import { take } from 'rxjs/operators';
 
-let command;
-let result;
 let inputFile;
 let inputText;
 
 beforeAll(() => {
-  command = new Command();
   inputFile = new File(['This is a test'], 'testFile');
   inputText = 'This is a text';
 });
 
-afterEach(() => {
-  result.unsubscribe();
-});
-
-test('result has a stdout', (done) => {
-  command.run('version');
-  result = command.resultAsObservable.subscribe((value) => {
-    expect(value.stdout).toBeTruthy();
-    done();
+describe('test if result observable emits values', () => {
+  test('result has a stdout', (done) => {
+    const command = new Command();
+    command.run('version');
+    command.resultAsObservable.pipe(take(1)).subscribe((value) => {
+      expect(value.stdout).toBeTruthy();
+      done();
+    });
   });
-});
 
-test('result has a stderr', (done) => {
-  command.run('help');
-  result = command.resultAsObservable.subscribe((value) => {
-    expect(value.stderr).toBeTruthy();
-    done();
+  test('result has a stderr', (done) => {
+    const command = new Command();
+    command.run('help');
+    command.resultAsObservable.pipe(take(1)).subscribe((value) => {
+      expect(value.stderr).toBeTruthy();
+      done();
+    });
   });
-});
 
-test('result has a text', (done) => {
-  command.run('enc -base64 -in file -out output', 'This is a test');
-  result = command.resultAsObservable.subscribe((value) => {
-    expect(value.text).toBeTruthy();
-    done();
+  test('result has a text', (done) => {
+    const command = new Command();
+    command.run('enc -base64 -in file -out output', inputText);
+    command.resultAsObservable.pipe(take(1)).subscribe((value) => {
+      expect(value.text).toBeTruthy();
+      done();
+    });
   });
-});
 
-test('result has a file', (done) => {
-  command.run('enc -base64 -in file -out output', inputFile);
-  result = command.resultAsObservable.subscribe((value) => {
-    expect(value.file).toBeTruthy();
-    done();
+  test('result has a file', (done) => {
+    const command = new Command();
+    command.run('enc -base64 -in file -out output', inputFile);
+    command.resultAsObservable.pipe(take(1)).subscribe((value) => {
+      expect(value.file).toBeTruthy();
+      done();
+    });
   });
-});
 
-test('result has a file with a specific name', (done) => {
-  command.run(`enc -base64 -in file -out ${inputFile.name}.enc`, inputFile);
-  result = command.resultAsObservable.subscribe((value) => {
-    expect(value.file.name).toBe(`${inputFile.name}.enc`);
-    done();
+  test('result has a file with a specific name', (done) => {
+    const command = new Command();
+    command.run(`enc -base64 -in file -out ${inputFile.name}.enc`, inputFile);
+    command.resultAsObservable.pipe(take(1)).subscribe((value) => {
+      expect(value.file.name).toBe(`${inputFile.name}.enc`);
+      done();
+    });
   });
 });
 
 describe('symmetric encryption methods with an input file', () => {
   test('aes-256-cbc encryption and decryption', (done) => {
+    const command = new Command();
     command.run(`enc -e -aes-256-cbc -k 1234 -in file -out ${inputFile.name}.enc`, inputFile);
-    result = command.resultAsObservable.subscribe((value) => {
+    command.resultAsObservable.pipe(take(2)).subscribe((value) => {
       if (value.file.name === `${inputFile.name}.enc`) {
         command.run(`enc -d -aes-256-cbc -k 1234 -in file -out testFile`, value.file);
       }
@@ -70,8 +72,9 @@ describe('symmetric encryption methods with an input file', () => {
   });
 
   test('camellia-256-cbc encryption and decryption', (done) => {
+    const command = new Command();
     command.run(`enc -e -camellia-256-cbc -k 1234 -in file -out ${inputFile.name}.enc`, inputFile);
-    result = command.resultAsObservable.subscribe((value) => {
+    command.resultAsObservable.pipe(take(2)).subscribe((value) => {
       if (value.file.name === `${inputFile.name}.enc`) {
         command.run(`enc -d -camellia-256-cbc -k 1234 -in file -out testFile`, value.file);
       }
@@ -83,8 +86,9 @@ describe('symmetric encryption methods with an input file', () => {
   });
 
   test('des3 encryption and decryption', (done) => {
+    const command = new Command();
     command.run(`enc -e -des3 -k 1234 -in file -out ${inputFile.name}.enc`, inputFile);
-    result = command.resultAsObservable.subscribe((value) => {
+    command.resultAsObservable.pipe(take(2)).subscribe((value) => {
       if (value.file.name === `${inputFile.name}.enc`) {
         command.run(`enc -d -des3 -k 1234 -in file -out testFile`, value.file);
       }
@@ -96,8 +100,9 @@ describe('symmetric encryption methods with an input file', () => {
   });
 
   test('des-ede3-cbc encryption and decryption', (done) => {
+    const command = new Command();
     command.run(`enc -e -des-ede3-cbc -k 1234 -in file -out ${inputFile.name}.enc`, inputFile);
-    result = command.resultAsObservable.subscribe((value) => {
+    command.resultAsObservable.pipe(take(2)).subscribe((value) => {
       if (value.file.name === `${inputFile.name}.enc`) {
         command.run(`enc -d -des-ede3-cbc -k 1234 -in file -out testFile`, value.file);
       }
@@ -111,8 +116,9 @@ describe('symmetric encryption methods with an input file', () => {
 
 describe('symmetric encryption methods with an input text in a base64 format', () => {
   test('aes-256-cbc encryption and decryption', (done) => {
+    const command = new Command();
     command.run(`enc -e -a -aes-256-cbc -k 1234 -in file -out output`, inputText);
-    result = command.resultAsObservable.subscribe((value) => {
+    command.resultAsObservable.pipe(take(2)).subscribe((value) => {
       if (value.text !== inputText) {
         command.run(`enc -d -a -aes-256-cbc -k 1234 -in file -out output`, value.text);
       }
@@ -124,8 +130,9 @@ describe('symmetric encryption methods with an input text in a base64 format', (
   });
 
   test('camellia-256-cbc encryption and decryption', (done) => {
+    const command = new Command();
     command.run(`enc -e -a -camellia-256-cbc -k 1234 -in file -out output`, inputText);
-    result = command.resultAsObservable.subscribe((value) => {
+    command.resultAsObservable.pipe(take(2)).subscribe((value) => {
       if (value.text !== inputText) {
         command.run(`enc -d -a -camellia-256-cbc -k 1234 -in file -out output`, value.text);
       }
@@ -137,8 +144,9 @@ describe('symmetric encryption methods with an input text in a base64 format', (
   });
 
   test('des3 encryption and decryption', (done) => {
+    const command = new Command();
     command.run(`enc -e -a -des3 -k 1234 -in file -out output`, inputText);
-    result = command.resultAsObservable.subscribe((value) => {
+    command.resultAsObservable.pipe(take(2)).subscribe((value) => {
       if (value.text !== inputText) {
         command.run(`enc -d -a -des3 -k 1234 -in file -out output`, value.text);
       }
@@ -150,8 +158,9 @@ describe('symmetric encryption methods with an input text in a base64 format', (
   });
 
   test('des-ede3-cbc encryption and decryption', (done) => {
+    const command = new Command();
     command.run(`enc -e -a -des-ede3-cbc -k 1234 -in file -out output`, inputText);
-    result = command.resultAsObservable.subscribe((value) => {
+    command.resultAsObservable.pipe(take(2)).subscribe((value) => {
       if (value.text !== inputText) {
         command.run(`enc -d -a -des-ede3-cbc -k 1234 -in file -out output`, value.text);
       }
@@ -159,6 +168,17 @@ describe('symmetric encryption methods with an input text in a base64 format', (
         expect(value.text).toEqual(inputText);
         done();
       }
+    });
+  });
+});
+
+describe('asymmetric rsa encryption', () => {
+  test('rsa 2048 bit computation', (done) => {
+    const command = new Command();
+    command.run('genrsa 2048');
+    command.resultAsObservable.pipe(take(1)).subscribe((value) => {
+      expect(value.stdout).toBeTruthy();
+      done();
     });
   });
 });
