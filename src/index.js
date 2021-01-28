41d2299ff049ba8ae04e63180bf8e33586b04fbd
Jan Neumann <66623604+janeumnn@users.noreply.github.com>
2021-01-29 00:13:55 +0100

    Implement CommandLine component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
