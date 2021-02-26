9d1fa9a1eea51a31527721d5efbb686ce3e8fcff
Jan Neumann <66623604+janeumnn@users.noreply.github.com>
2021-02-26 01:02:59 +0100

    Create context to store data

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -3,6 +3,7 @@ import { FormFile } from 'react-bootstrap';
 import CardControl from '../card-control/CardControl';
 import CommandLine from '../../components/command-line/CommandLine';
 import Command from '../../core/command';
+import { useStore } from '../../contexts/store';
 import { downloadFile } from '../../utils/downloadFile';
 
 import './App.css';
@@ -14,13 +15,13 @@ const delay = (ms) => {
 };
 
 function App() {
+  const { dispatch } = useStore();
   const [output, setOutput] = useState({ stdout: '', stderr: '', text: '', file: null });
   const [inputFiles, setInputFiles] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
 
   const runCommand = async (args) => {
-    setIsLoading(true);
-    await delay(10);
+    dispatch({ type: 'SET_LOADING', isLoading: true });
+    await delay(30);
     if (inputFiles.length) {
       await command.run(args, inputFiles);
     } else {
@@ -31,7 +32,7 @@ function App() {
   useEffect(() => {
     const result = command.resultAsObservable.subscribe((value) => {
       if (value) {
-        setIsLoading(false);
+        dispatch({ type: 'SET_LOADING', isLoading: false });
         setOutput(value);
         if (value.file) {
           downloadFile(value.file, value.file.name, null);
@@ -42,21 +43,29 @@ function App() {
     return () => {
       result.unsubscribe();
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  const handleFileInputChange = (event) => {
+    setInputFiles([...event.target.files]);
+    dispatch({
+      type: 'SET_FILENAMES',
+      fileNames: [...event.target.files].map((file) => file.name),
+    });
+  };
 
   return (
     <div className="App">
       <CardControl></CardControl>
       <FormFile className="mt-3 mb-3" custom>
-        <FormFile.Input onChange={(e) => setInputFiles([...e.target.files])} multiple />
+        <FormFile.Input onChange={handleFileInputChange} multiple />
         <FormFile.Label data-browse="Browse...">
-          {inputFiles.length ? inputFiles.map((f) => f.name).join(', ') : 'Select files...'}
+          {inputFiles.length ? inputFiles.map((file) => file.name).join(', ') : 'Select files...'}
         </FormFile.Label>
       </FormFile>
       <CommandLine
         runCommand={runCommand}
         result={{ stdout: output.stdout, stderr: output.stderr, text: output.text }}
-        isLoading={isLoading}
       ></CommandLine>
     </div>
   );
