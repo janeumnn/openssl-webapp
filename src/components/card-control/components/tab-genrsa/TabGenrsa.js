82e9c550079396a06402d4ab2f5b1bdbdf61f098
janeumnn <jan.neumann@ensun.de>
2021-03-01 21:32:02 +0100

    Change validation behavior

--- src/components/card-control/components/tab-genrsa/TabGenrsa.js
+++ src/components/card-control/components/tab-genrsa/TabGenrsa.js
@@ -8,6 +8,9 @@ const NUMBITS = ['1024', '2048', '4096'];
 
 function TabGenrsa({ runCommand }) {
   const { state, dispatch } = useStore();
+  const [validation, setValidation] = useState({
+    fileOutput: false,
+  });
   const [genrsa, setGenrsa] = useState({
     out: false,
     outFile: 'id_rsa',
@@ -21,15 +24,12 @@ function TabGenrsa({ runCommand }) {
         case 'out':
           if (genrsa.out) command.push('-out');
           break;
-
         case 'outFile':
           if (genrsa.out) command.push(genrsa.outFile);
           break;
-
         case 'numbits':
           command.push(genrsa.numbits);
           break;
-
         default:
           break;
       }
@@ -39,18 +39,41 @@ function TabGenrsa({ runCommand }) {
 
   const set = (key) => (event) => {
     const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
-    setGenrsa((prev) => ({ ...prev, [key]: value }));
+    switch (key) {
+      case 'out':
+        setGenrsa((prev) => ({ ...prev, [key]: value }));
+        setValidation((prev) => ({ ...prev, fileOutput: false }));
+        break;
+      case 'outFile':
+        setGenrsa((prev) => ({ ...prev, [key]: value }));
+        setValidation((prev) => ({ ...prev, fileOutput: false }));
+        break;
+      default:
+        setGenrsa((prev) => ({ ...prev, [key]: value }));
+        break;
+    }
+  };
+
+  const checkValidation = () => {
+    let valid = true;
+    if (!genrsa.outFile) {
+      setValidation((prev) => ({ ...prev, fileOutput: true }));
+      valid = false;
+    }
+    return valid;
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const command = commandBuilder();
-    dispatch({ type: 'SET_COMMAND', command: command });
-    runCommand(command);
+    if (checkValidation()) {
+      const command = commandBuilder();
+      dispatch({ type: 'SET_COMMAND', command: command });
+      runCommand(command);
+    }
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
+    <Form noValidate onSubmit={handleSubmit}>
       <Form.Row className="justify-content-start">
         <Col md={2}>
           <Form.Group>
@@ -67,7 +90,9 @@ function TabGenrsa({ runCommand }) {
               value={genrsa.outFile}
               onChange={set('outFile')}
               disabled={!genrsa.out}
+              isInvalid={validation.fileOutput}
             ></Form.Control>
+            <Form.Control.Feedback type="invalid">No text input</Form.Control.Feedback>
           </Form.Group>
         </Col>
         <Col md={2}>
