3c37abbd68da99a13c49da1170ddaa5b3a638414
Jan Neumann <66623604+janeumnn@users.noreply.github.com>
2021-05-31 22:46:12 +0200

    Add command line input history

--- src/components/command-line/CommandLine.js
+++ src/components/command-line/CommandLine.js
@@ -1,17 +1,66 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useStore } from '../../contexts/store';
 
 import './CommandLine.css';
 
 function CommandLine({ runCommand, result }) {
   const { state, dispatch } = useStore();
+  const [command, setCommand] = useState('');
   const input = useRef();
+  const commandHistory = useRef({
+    selectedIndex: -1,
+    commands: [],
+  });
+
+  const addToCommandHistory = (command) => {
+    if (commandHistory.current.commands.findIndex((x) => x === command) === -1)
+      commandHistory.current.commands.unshift(command);
+    if (commandHistory.current.length > 10) commandHistory.current.commands.pop();
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    dispatch({ type: 'SET_COMMAND', command: input.current.value });
-    runCommand(input.current.value);
-    input.current.value = '';
+    if (input.current.value.replace(/\s/g, '')) {
+      addToCommandHistory(input.current.value);
+      dispatch({ type: 'SET_COMMAND', command: input.current.value });
+      runCommand(input.current.value);
+      setCommand('');
+      commandHistory.current.selectedIndex = -1;
+    }
+  };
+
+  const handleOnChange = (event) => {
+    setCommand(event.target.value);
+    commandHistory.current.selectedIndex = -1;
+  };
+
+  const handleOnKeyDown = (event) => {
+    let selectedIndex = commandHistory.current.selectedIndex;
+    switch (event.key) {
+      case 'ArrowUp':
+        event.preventDefault();
+        selectedIndex += 1;
+        if (selectedIndex < 10 && commandHistory.current.commands[selectedIndex]) {
+          input.current.value = commandHistory.current.commands[selectedIndex];
+          commandHistory.current.selectedIndex = selectedIndex;
+        }
+        return;
+      case 'ArrowDown':
+        event.preventDefault();
+        selectedIndex -= 1;
+        if (selectedIndex >= 0 && commandHistory.current.commands[selectedIndex]) {
+          input.current.value = commandHistory.current.commands[selectedIndex];
+          commandHistory.current.selectedIndex = selectedIndex;
+        }
+
+        if (selectedIndex === -1) {
+          input.current.value = command;
+          commandHistory.current.selectedIndex = selectedIndex;
+        }
+        return;
+      default:
+        return;
+    }
   };
 
   return (
@@ -32,7 +81,14 @@ function CommandLine({ runCommand, result }) {
       <form onSubmit={handleSubmit}>
         <div className="CommandLine-input">
           <label>OpenSSL&gt;</label>
-          <input ref={input} type="text" spellCheck="false"></input>
+          <input
+            ref={input}
+            type="text"
+            spellCheck="false"
+            value={command}
+            onChange={handleOnChange}
+            onKeyDown={handleOnKeyDown}
+          ></input>
         </div>
       </form>
     </div>
